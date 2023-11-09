5ac19b037f4bbfb925028392649645779f412f24
Bhuvaneshwari <Bhuvaneshwari@LAPTOP-AJID0SHN>
2023-11-09 15:11:13 +0800

    Completed WelcomePage, AccountsPage and CasesPage Created CreateAccountsPage, EditAccountsPage, DeleteAccountsPage CreateCasePage, EditCasePage and DeleteCasePage

--- SalesforceCloud/src/test/java/com/sales/cloud/pages/AccountsPage.java
+++ SalesforceCloud/src/test/java/com/sales/cloud/pages/AccountsPage.java
@@ -1,5 +1,37 @@
 package com.sales.cloud.pages;
 
-public class AccountsPage {
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
 
+
+
+public class AccountsPage extends ProjectSpecificMethods {
+
+	public CreateAccountsPage clickNewButton() {
+		clickUsingJs(locateElement(Locators.XPATH, "//div[@title='New']"));
+		return new CreateAccountsPage();
+	}
+	
+	public AccountsPage searchInput(String accName) {
+		typeAndEnter(locateElement(Locators.XPATH, "//input[@placeholder='Search this list...']"), accName);
+		return this;
+	}
+	
+	public AccountsPage clickDropdown() {
+		clickUsingJs(locateElement(Locators.XPATH, "//div[@data-aura-class='forceVirtualAction']/*"));
+		return this;
+	}
+	
+	public EditAccountPage clickEdit() {
+		clickUsingJs(locateElement(Locators.XPATH, "//a[@title='Edit']"));
+		return new EditAccountPage();
+	}
+	
+	public DeleteAccountPage clickDelete() {
+		clickUsingJs(locateElement(Locators.XPATH, "//a[@title='Delete']"));
+		return new DeleteAccountPage();
+	}
+	
+	
+	
 }
